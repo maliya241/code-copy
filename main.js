f4fe77545b1579a46deb35ca12d3087213b4691f
Maria McConkey <maliya241@users.noreply.github.com>
2021-01-27 11:04:39 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 function copyCode(element) {
+	// var temp_textarea_element = document.createElement("TEXTAREA");
 	var $temp = $("<textarea>");
+	
+	// document.body.appendChild(temp_textarea_element);
 	$("body").append($temp);
+	
+	// var copyText = document.getElementById("beginningHTML").textContent;
+	// temp_textarea_element.value = copyText.select();
 	$temp.val($(element).text()).select();
+	
 	document.execCommand("copy");
+	
+	//temp_textarea_element.remove();
 	$temp.remove();
 }
+
